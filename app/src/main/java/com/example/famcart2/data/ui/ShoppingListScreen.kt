00ed1e759572916867ba92690eeca22ce6979693
Kevin Cho <kcho760@gmail.com>
2024-08-29 19:29:56 -0400

    working on sliding items

--- app/src/main/java/com/example/famcart2/data/ui/ShoppingListScreen.kt
+++ app/src/main/java/com/example/famcart2/data/ui/ShoppingListScreen.kt
@@ -1,9 +1,11 @@
 package com.example.famcart2.data.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -11,6 +13,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.famcart2.data.ui.viewmodel.ShoppingViewModel
@@ -24,7 +28,7 @@ fun ShoppingListScreen(
     val items by viewModel.allItems.collectAsState(initial = emptyList())
 
     Column(
-        modifier = modifier.fillMaxSize(), // Use the passed-in modifier
+        modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
@@ -59,7 +63,6 @@ fun ShoppingListScreen(
             Text("Add Item")
         }
 
-        // LazyColumn to display the list of items
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
@@ -75,8 +78,10 @@ fun ShoppingListScreen(
                         viewModel.deleteItem(item)
                     }
                 )
+                Spacer(modifier = Modifier.height(8.dp)) // Space between items
             }
         }
+
     }
 }
 
@@ -90,7 +95,10 @@ fun ShoppingListItem(
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp),
+            .shadow(20.dp, shape = RoundedCornerShape(8.dp))
+            .background(MaterialTheme.colorScheme.surface, shape = RoundedCornerShape(8.dp))
+            .border(3.dp, MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(8.dp))
+            .padding(6.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Checkbox(
